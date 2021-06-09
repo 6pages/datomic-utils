cca3216d9cb055f3c27190088798fb34ac245f95
Adam Tait <bin@adamta.it>
2021-06-09 15:43:27 -0700

    user: better example with schema and transactions

--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,14 @@
 (ns user
   (:require
+   [clojure.data.generators :as gen]
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
    [com.6pages.datomic :as d]
    [com.6pages.datomic.schema :as ds]
-   [com.6pages.datomic.transact :as dt]))
+   [com.6pages.datomic.transact :as dt])
+  (:import
+   [java.util Date UUID]))
+
 
 ;;
 ;; configuration
@@ -19,17 +25,59 @@
   ;; REPL session
 
   (def client (d/client client-cfg))
-  (def db-name
-    (rand-nth
-     (d/list-dbs {:client client})))
-  (def opts
-    {:client client :db-name db-name})
+  (def db-name "mbrainz-subset")
+  (def opts {:client client :db-name db-name})
 
+  ;; mbrainz schema: https://github.com/Datomic/mbrainz-sample/wiki/Schema
   (ds/get-schema opts)
 
-  (d/q opts '[:find (pull ?e [*])
-              :where [?e :user/email _]])
+  (d/q opts '[:find (count ?e)
+              :where [?e :label/name _]])
+
+  (d/p-> opts '[*] [[:label/name "Universal"]])
+
+  ,)
+
+
+
+(comment
+
+  (def client (d/client client-cfg))
+  (def db-name "dev")
+  (def schemas (-> "datomic/example/schema.edn"
+                   io/resource slurp
+                   edn/read-string))
+
+  (def opts {:client client :db-name db-name})
+  (def topts
+    {:schemas (flatten schemas)
+     :unique-attrs (ds/schemas->unique-attrs schemas)})
+
+  (d/create-db opts)
+  (ds/update! opts schemas)
+  (ds/get-schema opts)
+
+  (dt/entity->delta-facts
+   opts topts
+   {:person/id (UUID/randomUUID)
+    :person/name (gen/string)})
+
+  (def ids (repeatedly 3 #(UUID/randomUUID)))
+  (dt/entity->transact!
+   opts topts
+   {:person/id (first ids)
+    :person/name (gen/string)})
+
+  (d/p-> opts '[*] [[:person/id (first ids)]])
 
   
+  (dt/entity->transact!
+   opts topts
+   {:person/id (first ids)
+    :person/friend
+    [{:person/id (second ids)
+      :person/name (gen/string)}]})
+
+  (d/p-> opts '[*] [[:person/id (first ids)]])
   
   ,)
